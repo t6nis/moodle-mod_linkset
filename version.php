e8eb9364ca986e7aaf2e3df966c3a9f27a4963ea
Tõnis Tartes <t6nis20@gmail.com>
2017-05-02 10:38:09 +0300

    Version update

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$module->version   = 2014082500;    // If version == 0 then module will not be installed
-$module->requires  = 2010031900;    // Requires this Moodle version
-$module->cron      = 0;             // Period for cron to check this module (secs)
-$module->component = 'mod_linkset'; // To check on upgrade, that module sits in correct place
+$plugin->version   = 2014082500;    // If version == 0 then module will not be installed
+$plugin->requires  = 2010031900;    // Requires this Moodle version
+$plugin->cron      = 0;             // Period for cron to check this module (secs)
+$plugin->component = 'mod_linkset'; // To check on upgrade, that module sits in correct place
